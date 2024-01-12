fcf362e8a913760afad7793cafa998adfc85f81d
Rodrigo_vasquez <rvasquez26@gmail.com>
2024-01-12 12:38:20 -0300

    modifica estructura de js

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,5 @@
 /*formatea los valores y separa por puntos*/
-const separarPorPuntos = (numero) => {
-    return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
-};
+
 
 precio = 400000
 precioSpan = document.querySelector(".precio-inicial");
@@ -13,6 +11,10 @@ const botonMas = document.querySelector("#mas");
 const botonMenos = document.querySelector("#menos");
 const valorTotal = document.querySelector("#valor-total");
 const stock = document.querySelector(".stock");
+
+const separarPorPuntos = (numero) => {
+    return numero.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ".");
+};
 
 const aumentar = () => {
 
